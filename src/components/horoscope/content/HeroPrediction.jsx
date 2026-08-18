9610e2bc14053a93e75f1308a7074fc17abedc46
devloper07 <gopalssm9852@gmail.com>
2026-08-18 12:47:19 +0530

    fix: add missing motion import in HeroPrediction.jsx

--- src/components/horoscope/content/HeroPrediction.jsx
+++ src/components/horoscope/content/HeroPrediction.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { motion } from 'framer-motion';
 import ZodiacIcon from '@/components/ZodiacIcon';
 import Stars from './Stars';
 import { signs } from '../horoData';
